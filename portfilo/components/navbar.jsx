419927be8eff11d838d3b9337d335dc8fbe4975e
Pramish Bhusal <bpramish71@gmail.com>
2025-11-26 21:25:50 +0545

    hover err

--- portfilo/components/navbar.jsx
+++ portfilo/components/navbar.jsx
@@ -8,25 +8,25 @@ export default function Navbar(){
 		rounded-xl py-4 px-6 text-[#0b1120]/50"
 	>
      <div className="flex items-center justify-between">
-        <h1 className="ttext-lg sm:text-xl md:text-2xl font-semibold">Pramish</h1>
+        <h1 className="text-lg sm:text-xl md:text-2xl font-semibold">Pramish</h1>
 
         <div className="flex text-[#9b9b9b] w-1/2 justify-around ">
-			<ul className="list-none text-sm sm:text-base md:text-lg hover:text-[#e4e4e4]">
+			<ul className="list-none text-sm sm:text-base md:text-lg hover:text-[#0b1120]/60">
 				<li>
-					<a href="/" className="hover:text-[#0b1120]/50 text-sm sm:text-base md:text-lg hover:text-[#e4e4e4]">Home</a>
+					<a href="/" className="hover:text-[#0b1120]/60 text-sm sm:text-base md:text-lg hover:text-[#e4e4e4]">Home</a>
 				</li>
 			</ul>
-			<ul className="list-none text-sm sm:text-base md:text-lg hover:text-[#e4e4e4]">
+			<ul className="list-none text-sm sm:text-base md:text-lg hover:text-[#0b1120]/50">
 				<li>
 					<a href="/projects" className="hover:text-[#0b1120]/50">Projects</a>
 				</li>
 			</ul>
-			<ul className="list-none text-sm sm:text-base md:text-lg hover:text-[#e4e4e4]">
+			<ul className="list-none text-sm sm:text-base md:text-lg hover:text-[#0b1120]/50">
 				<li>
 					<a href="/skills" className="hover:text-[#0b1120]/50">Skills</a>
 				</li>
 			</ul>
-			<ul className="list-none text-sm sm:text-base md:text-lg hover:text-[#e4e4e4]">
+			<ul className="list-none text-sm sm:text-base md:text-lg hover:text-[#0b1120]/50">
 				<li>
 					<a href="/contact" className="hover:text-[#0b1120]/50">Contact</a>
 				</li>
