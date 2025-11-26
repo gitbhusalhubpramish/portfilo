c0ed3b2c9ac6eeff5c62296f0a57ee411d303cc5
Pramish Bhusal <bpramish71@gmail.com>
2025-11-26 20:20:33 +0545

    tailwind

--- portfilo/components/navbar.jsx
+++ portfilo/components/navbar.jsx
@@ -1,7 +1,13 @@
 export default function Navbar(){
 	return(
-	<nav className="fixed top-4 left-1/2 transform -translate-x-1/2 w-11/12 max-w-4xl z-50 backdrop-blur-md bg-white border border-white/20 rounded-xl py-4 px-6">
-      <div className="flex items-center justify-between">
+	<nav className="
+		fixed top-4 left-1/2 transform -translate-x-1/2
+		w-11/12 max-w-4xl z-50
+		backdrop-blur-md bg-white/90
+		border border-white/20
+		rounded-xl py-4 px-6"
+	>
+     <div className="flex items-center justify-between">
         <h1 className="text-xl font-semibold w-1/2">Pramish</h1>
 
         <div className="flex text-[#9b9b9b] w-1/2 justify-around">
