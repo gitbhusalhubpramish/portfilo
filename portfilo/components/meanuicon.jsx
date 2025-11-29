c8072fff05be101f30d1f059607a40fdc3840973
gitbhusalhubpramish <bpraimsh71@gmail.com>
2025-11-29 17:07:57 +0545

    meanu

--- portfilo/components/meanuicon.jsx
+++ portfilo/components/meanuicon.jsx
@@ -1,7 +1,10 @@
 export default function MeanuIcon(){
 	return (
 	<>
-		<svg viewBox="0 0 24 24" width="24" height="24" xmlns="www.w3.org"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm0-14c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-2.76 0-5 2.24-5 5s2.24 5 5 5 5-2.24 5-5-2.24-5-5-5z"/></svg>
+		<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M3 12H21M3 6H21M3 18H21" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+
 	</>
 	)
 }
