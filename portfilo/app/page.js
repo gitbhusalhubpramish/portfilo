71d1107551dca16a2ddaf024c9490504aa52027e
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2025-12-01 19:01:10 +0545

    menu handeller

--- portfilo/app/page.js
+++ portfilo/app/page.js
@@ -1,8 +1,16 @@
 import Image from "next/image";
 import Navbar from "../components/navbar";
+import Contant from "../components/homecontant";
 
 export default function Home() {
   return (
+  <>
+	<div>
     <Navbar/>
+    </div>
+    <div className="">
+    <Contant/>
+    </div>
+    </>
   );
 }
--- portfilo/components/navbar.jsx
+++ portfilo/components/navbar.jsx
@@ -1,20 +1,16 @@
+"use client"
+import {useState} from "react";
 import MeanuIcon from "./meanuicon.jsx";
 export default function Navbar() {
   const linkclass =
     "hover:text-[#0b1120]/60 text-sm sm:text-xs md:text-lg";
   const ulcls =
     "list-none text-sm sm:text-base md:text-lg text-[#9b9b9b]";
-
+  const navcls = "fixed top-4 left-1/2 -translate-x-1/2 w-11/12 max-w-4xl z-50 backdrop-blur-md bg-white/90 border border-white/20 rounded-xl py-4 px-6 text-[#0b1120]/50"
+  const [showmenu, setshowmenu] = useState(false);
+  const menuicnclk = ()=> setshowmenu(!showmenu);
   return (
-    <nav
-      className="
-        fixed top-4 left-1/2 -translate-x-1/2
-        w-11/12 max-w-4xl z-50
-        backdrop-blur-md bg-white/90
-        border border-white/20
-        rounded-xl py-4 px-6 text-[#0b1120]/50
-      "
-    >
+    <nav className={navcls}>
       <div className="flex items-center justify-between">
         <h1 className="mr-5 text-lg sm:text-xl md:text-2xl font-semibold">
           Pramish
@@ -51,7 +47,7 @@ export default function Navbar() {
             </li>
           </ul>
         </div>
-        <button className="sm:hidden border border-black rounded-md border-solid bg-black/10 cursor-pointer">
+        <button onClick = {menuicnclk} className="sm:hidden border border-black rounded-md border-solid bg-black/10 cursor-pointer">
 			<MeanuIcon/>
         </button>
       </div>
