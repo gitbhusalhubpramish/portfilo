4deb4fee9f64a2479459544d8a0c18bc929ceff8
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2025-12-01 19:43:31 +0545

    bg

--- portfilo/components/navbar.jsx
+++ portfilo/components/navbar.jsx
@@ -6,10 +6,14 @@ export default function Navbar() {
     "hover:text-[#0b1120]/60 text-sm sm:text-xs md:text-lg";
   const ulcls =
     "list-none text-sm sm:text-base md:text-lg text-[#9b9b9b]";
-  const navcls = "fixed top-4 left-1/2 -translate-x-1/2 w-11/12 max-w-4xl z-50 backdrop-blur-md bg-white/90 border border-white/20 rounded-xl py-4 px-6 text-[#0b1120]/50"
+  const navcls = "fixed top-4 left-1/2 -translate-x-1/2 w-11/12 max-w-4xl z-50 backdrop-blur-md bg-white/90 border border-white/20 rounded-xl py-4 px-6 text-[#0b1120]/50 z-3"
   const [showmenu, setshowmenu] = useState(false);
   const menuicnclk = ()=> setshowmenu(!showmenu);
   return (
+  <>
+  {showmenu &&(
+  <div className="fixed inset-0 bg-black/50 z-60" onClick={()=> setshowmenu(false)}></div>
+  )}
     <nav className={navcls}>
       <div className="flex items-center justify-between">
         <h1 className="mr-5 text-lg sm:text-xl md:text-2xl font-semibold">
@@ -50,7 +54,10 @@ export default function Navbar() {
         <button onClick = {menuicnclk} className="sm:hidden border border-black rounded-md border-solid bg-black/10 cursor-pointer">
 			<MeanuIcon/>
         </button>
+        
       </div>
     </nav>
+    
+    </>
   );
 }
