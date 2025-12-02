ee76a4762cd768f5a72a988880c178a9031a86bc
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2025-12-02 20:04:37 +0545

    menu

--- portfilo/components/menu.jsx
+++ portfilo/components/menu.jsx
@@ -1,12 +1,17 @@
 import navLinks from "../data/navlinks.json";
+import Link from 'next/link';
 export default function Menu(){
 	return(
-		<div className="h-full w-1/3 flex flex-col">
+		<div className="h-full w-2/3 flex flex-col bg-[#1a1918]">
 			{navLinks.map((item, index)=>{
 				return(
-					<button key={index} className="">
-					<a href={item.href}>{item.name}</a>
+					<button
+						key={index}
+						className="px-6 py-3 text-lg rounded-lg hover:bg-white/10 transition"
+					>
+						<Link href={item.href}>{item.name}</Link>
 					</button>
+
 				)
 			})}
 		</div>
