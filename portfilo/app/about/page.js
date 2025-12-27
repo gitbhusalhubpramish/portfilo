26dfafdfa2eb05e776a42eb6a79599274770e318
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2025-12-27 21:07:27 +0545

    about data

--- portfilo/app/about/page.js
+++ portfilo/app/about/page.js
@@ -1,7 +1,20 @@
-export default function About(){
-	return(
-		<>
-		
-		</>
-	)
+import data from "@/data/about.json"
+import React from "react";
+
+export default function About() {
+	/*const elm = (dta)=>(
+		{}
+	)*/
+  return (
+    <div className="m-45">
+      {data.data.map((item, index) => (
+        <div key={index} className="m-2">
+          <h1 className="text-xl">{item.Heading}</h1>
+          {item.des.map((itm, idx) =>
+            React.createElement(itm.tag, { key: idx }, itm.elm)
+          )}
+        </div>
+      ))}
+    </div>
+  )
 }
