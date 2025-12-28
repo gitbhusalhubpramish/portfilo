3d4df8c20d1eed818c50acd12b85364bd3994345
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2025-12-28 19:26:06 +0545

    cls

--- portfilo/app/about/page.js
+++ portfilo/app/about/page.js
@@ -5,14 +5,14 @@ export default function About() {
 	const renderElem = (node, key) => {
     // base case: text node
     if (typeof node.elm === "string") {
-      return React.createElement(node.tag, { key }, node.elm);
+      return React.createElement(node.tag, { key, className:node.cls }, node.elm);
     }
 
     // recursive case: array of children
     if (Array.isArray(node.elm)) {
       return React.createElement(
         node.tag,
-        { key },
+        { key, className:node.cls },
         node.elm.map((child, idx) => renderElem(child, idx))
       );
     }
