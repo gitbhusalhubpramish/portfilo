a496d4a99bd47925cbc027fc531c7bb9ff076f82
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2025-12-29 20:51:53 +0545

    design

--- portfilo/app/about/page.js
+++ portfilo/app/about/page.js
@@ -2,17 +2,17 @@ import data from "@/data/about.json"
 import React from "react";
 
 export default function About() {
-	const renderElem = (node, key) => {
+	const renderElem = (node, keyPrefix = "root") => {
     // base case: text node
     if (typeof node.elm === "string") {
-      return React.createElement(node.tag, { key, className:node.cls }, node.elm);
+      return React.createElement(node.tag, { key: keyPrefix, className:node.cls }, node.elm);
     }
 
     // recursive case: array of children
     if (Array.isArray(node.elm)) {
       return React.createElement(
         node.tag,
-        { key, className:node.cls },
+        { key:keyPrefix, className:node.cls },
         node.elm.map((child, idx) => renderElem(child, idx))
       );
     }
@@ -20,10 +20,10 @@ export default function About() {
     return null;
   };
   return (
-    <div className="m-45">
+    <div className="sm:m-50 mx-0 m-30">
       {data.data.map((item, index) => (
-        <div key={index} className="m-2 ">
-          <h1 className="text-3xl">{item.Heading}</h1>
+        <div key={index} className="m-10 text-gray-300">
+          <h1 className="text-5xl font-bold text-[#33a400]">{item.Heading}</h1>
           {item.des.map((itm, idx) =>
             renderElem(itm)
           )}
