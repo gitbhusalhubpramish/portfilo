29bdffbe7896c3164eefc463c066f6f798826104
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-01-09 18:07:53 +0545

    card skilll

--- portfilo/app/about/page.js
+++ portfilo/app/about/page.js
@@ -1,11 +1,25 @@
+import {Metadata} from "next"
 import data from "@/data/about.json"
 import React from "react";
 
+export const metadata = {
+  title: "About me",
+  description: "Portfolio of Pramish Bhusal, a Nepali developer skilled in JavaScript, Python, C++, and more. Projects, blogs, and contact.",
+  keywords: [
+      "Pramish Bhusal",
+      "Nepal developer",
+      "13 years old prodigi",
+      "Pygame chess",
+      "JavaScript developer Nepal",
+      "Python projects",
+      "Next.js portfolio"
+  ]
+};
 export default function About() {
 	const renderElem = (node, keyPrefix = "root") => {
     // base case: text node
     if (typeof node.elm === "string") {
-      return React.createElement(node.tag, { key: keyPrefix, className:node.cls }, node.elm);
+      return React.createElement(node.tag, { key: keyPrefix, className:node.cls, src:node.src?node.src:null, alt:node.alt?node.alt:null}, node.elm);
     }
 
     // recursive case: array of children
