c47cba83f66a130669b4afddc2d3df5d6ca70ddf
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-01-17 18:07:07 +0545

    hktn

--- portfilo/app/about/achivements.js
+++ portfilo/app/about/achivements.js
@@ -1,28 +1,56 @@
-import data from "@/data/about/achivements.json"
-export default function Achivements(){
+import data from "@/data/about/achivements.json";
+import Image from "next/image";
+
+export default function Achivements() {
 	return (
 		<div className="m-10 text-gray-300">
-			<h1 className="text-5xl font-bold text-[#33a400]">{data.Heading}</h1>
-			
-			<div className="mt-4 flex bg-[#121830] text-[#e0e0e0] overflow-x-auto gap-5 sm:gap-10 pt-4 pb-7 px-7 rounded-2xl relative shadow-[inset_0_8px_15px_#0b1120,inset_0_-8px_15px_#0b1120,inset_0_0_30px_#0b1120] ">
-				{data.des.map((item,index)=>(
-					<div key={index} className="bg-[#1f2940] w-full flex flex-wrap gap-2 flex-shrink-0 sm:p-7 p-5 border border-[#3d5afe] rounded-3xl transition-transform hover:bg-[#26314f] shadow-lg shadow-[#0b1120]/50">
-						<div className="flex flex-wrap-reverse items-center gap-3">
-							<h1 className="flex-1 text-xl font-bold text-white break-words">
+			<h1 className="text-5xl font-bold text-[#33a400]">
+				{data.Heading}
+			</h1>
+
+			<div className="mt-4 flex bg-[#121830] text-[#e0e0e0] gap-5 sm:gap-10 pt-4 pb-7 px-7 rounded-2xl relative shadow-[inset_0_8px_15px_#0b1120,inset_0_-8px_15px_#0b1120,inset_0_0_30px_#0b1120] flex">
+				{data.des.map((item, index) => (
+					<div
+						key={index}
+						className="bg-[#1f2940] w-full flex flex-col gap-3 flex-shrink-0  border border-[#3d5afe] rounded-3xl transition-transform min-h-screen shadow-lg shadow-[#0b1120]/50 overflow-hidden "
+					>
+						{/* Background Image */}
+						{/*{item.bgimg && (*/}
+							<div className="w-full flex-1 relative  overflow-hidden min-h-[200px] sm:min-h-[300px]">
+								<Image
+									src={item.bgimg}
+									alt="achievement cover"
+									fill
+									sizes="100%, 100%"
+									className="object-cover"
+								/>
+								<div className="absolute inset-0 bg-gradient-to-b from-transparent to-[#1f2940]" />
+							</div>
+						{/*)}*/}
+
+						{/* Title */}
+						<div className="mx-5 sm:mx-7 mt-10 mb-15">
+						<div className="flex items-center gap-3">
+							<h2 className="text-3xl font-bold text-white break-words">
 								{item.title}
-							</h1>
+							</h2>
 						</div>
 
-
-
-						<div className="flex m-1">
-							<div className="w-2 mr-1 bg-gray-600"/>
-							<p className="text-[#e0e0e0]">{item.des}</p>
+						{/* Description */}
+						<div className="flex mt-1 ml-1">
+							<div className="w-1 mr-2 bg-gray-600 rounded-full" />
+							<div>
+							{item.des.map((par,idx)=>(
+							<p key={idx} className="text-[#e0e0e0] mt-2 text-sm leading-relaxed">
+								{par}
+							</p>
+							))}
+							</div>
 						</div>
-						
+						</div>
 					</div>
 				))}
 			</div>
 		</div>
-	)
+	);
 }
