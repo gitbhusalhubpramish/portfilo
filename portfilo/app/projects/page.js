85ccc9d49d629dcbd25259ccb797526a7f9f95f0
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-01-20 20:27:34 +0545

    detail

--- portfilo/app/projects/page.js
+++ portfilo/app/projects/page.js
@@ -16,10 +16,12 @@ export default function Project(){
 	return (
 		<div className="min-h-screen sm:m-[15%] mx-0 m-30">
 			{data.projects.map((item,index)=>(
-				<div className="m-10 text-gray-300">
+				<div key={index} className="m-10 text-gray-300">
 					<h1 className="text-5xl font-bold text-[#33a400]">{item.topic} Project</h1>
 					<div className="mt-4 flex flex-wrap bg-[#121830] text-[#e0e0e0] gap-5 sm:gap-10 pt-4 pb-7 px-7 rounded-2xl relative shadow-[inset_0_8px_15px_#0b1120,inset_0_-8px_15px_#0b1120,inset_0_0_30px_#0b1120] flex">
-					
+						{item.list.map((project,index)=>(
+							<details key={index}></details>
+						))}
 					</div>
 				</div>
 			))}
