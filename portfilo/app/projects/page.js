c8523ca283c60d587d4688d7045cdb8f470f10c2
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-01-20 19:22:27 +0545

    page

---
+++ portfilo/app/projects/page.js
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "My projects",
+  description: "Portfolio of Pramish Bhusal, a Nepali developer skilled in JavaScript, Python, C++, and more. Projects, blogs, and contact.",
+  keywords: [
+      "Pramish Bhusal",
+      "Nepal developer",
+      "13 years old prodigi",
+      "Pygame chess",
+      "JavaScript developer Nepal",
+      "Python projects",
+      "Next.js portfolio"
+  ]
+};
+export default function Project(){
+	
+}
