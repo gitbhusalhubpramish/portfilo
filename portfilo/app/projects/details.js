e584160329746b57d515449b138e6cfcd21aa9e5
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-01-25 19:38:49 +0545

    file

---
+++ portfilo/app/projects/details.js
@@ -0,0 +1,22 @@
+"use client"
+export default function Details({project}){
+	return (
+		<details className="bg-[#1f2940] w-full flex flex-col gap-3 flex-shrink-0  border border-[#3d5afe] rounded-3xl transition-transform  shadow-lg shadow-[#0b1120]/50 overflow-hidden group">
+			<summary className="list-none mb-15 [&::marker]:hidden mx-5 sm:mx-7 cursor-pointer">
+				<div className=" mt-10 flex">
+					<h1 className="text-3xl font-bold text-[#33a400]/85 flex-1">{project.title}</h1>
+					<div className="w-1/5 flex items-center justify-center">
+						<div
+							className="h-10 w-10 rotate-45 border-b-2 border-r-2 transition-transform group-open:-rotate-[135deg]"
+						/>
+					</div>
+				</div>
+				<div className="flex">
+					<div className="w-1 bg-gray-600"/>
+						<p className="ml-2 text-gray-300">hello</p>
+					</div>
+			</summary>
+			<div>hello</div>
+		</details>
+	)
+}
--- portfilo/app/projects/page.js
+++ portfilo/app/projects/page.js
@@ -1,4 +1,6 @@
+"client site"
 import data from "@/data/project.json"
+import Details from "./details.js"
 export const metadata = {
   title: "My projects",
   description: "Portfolio of Pramish Bhusal, a Nepali developer skilled in JavaScript, Python, C++, and more. Projects, blogs, and contact.",
@@ -15,23 +17,10 @@ export const metadata = {
 export default function Project(){
 	return (
 		<div className="min-h-screen sm:m-[15%] mx-0 m-30">
-			{data.projects.map((item,index)=>(
+			{data.projects.map((project,index)=>(
 				<div key={index} className="m-10 text-gray-300">
-					<h1 className="text-5xl font-bold text-[#33a400]">{item.topic} Project</h1>
 					<div className="mt-4 flex flex-wrap bg-[#121830] text-[#e0e0e0] gap-5 sm:gap-10 pt-4 pb-7 px-7 rounded-2xl relative shadow-[inset_0_8px_15px_#0b1120,inset_0_-8px_15px_#0b1120,inset_0_0_30px_#0b1120] flex">
-						{item.list.map((project,index)=>(
-							<details className="bg-[#1f2940] w-full flex flex-col gap-3 flex-shrink-0  border border-[#3d5afe] rounded-3xl transition-transform  shadow-lg shadow-[#0b1120]/50 overflow-hidden group" key={index}>
-								<summary className="list-none [&::marker]:hidden flex cursor-pointer">
-									<div className="mx-5 sm:mx-7 mt-10 mb-15 flex">
-									<h1 className="text-3xl font-bold text-[#33a400]/85 flex-1">{project.title}</h1>
-									<div className="w-1/5 flex items-center justify-center">
-									<div
-										className="h-10 w-10 rotate-45 border-b-2 border-r-2 transition-transform group-open:-rotate-[135deg]"
-									/></div></div>
-								</summary>
-								<div>hello</div>
-							</details>
-						))}
+						<Details project={project}/>
 					</div>
 				</div>
 			))}
