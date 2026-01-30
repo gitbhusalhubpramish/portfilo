acfc0b185bdede712df46564c6895ea95f1fd2c2
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-01-30 17:10:42 +0545

    list

--- portfilo/app/projects/details.js
+++ portfilo/app/projects/details.js
@@ -38,7 +38,7 @@ export default function Details({project}){
 					<p className="ml-2 text-gray-300 text-sm">{des}</p>
 					</div>
 			</summary>
-			<div className="mx-5 sm:mx-7 mb-16 prose prose-invert max-w-none[&>h1]:text-3xl [&>h1]:font-bold [&>h2]:text-2xl [&>h3]:text-xl [&>pre]:bg-[#343541] [&>pre]:rounded-lg [&>pre]:p-4 [&>pre]:text-white [&>code]:bg-[#343541] [&>code]:rounded-lg [&>code]:p-1 [&>code]:text-white [&>*]:m-1">
+			<div className="mx-5 sm:mx-7 mb-16 prose prose-invert max-w-none [&>h1]:text-3xl [&>h1]:font-bold [&>h2]:text-2xl [&>h3]:text-xl [&>pre]:bg-[#343541] [&>pre]:rounded-lg [&>pre]:p-4 [&>pre]:text-white [&_code]:bg-[#343541] [&_code]:rounded-lg [&_code]:p-1 [&_code]:text-white [&>*]:m-1 [&>h1]:mt-4 [&>h2]:mt-3 [&>h3]-mt-2 [&>ul]:list-disc [&>ul]:ml-10">
 
 				<ReactMarkdown remarkPlugins={[remarkGfm]}>
 					{readme ?? ""}
