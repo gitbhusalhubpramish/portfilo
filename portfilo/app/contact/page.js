144a1d48ea60c4605bcc23abb41a8fcc529a3694
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-02-16 19:48:26 +0545

    responsive me

--- portfilo/app/contact/page.js
+++ portfilo/app/contact/page.js
@@ -7,17 +7,17 @@ export default function Contact(){
 					<div className="mt-4 flex flex-wrap bg-[#121830] text-[#e0e0e0] gap-5 sm:gap-10 pt-4 pb-7 px-7 rounded-2xl relative shadow-[inset_0_8px_15px_#0b1120,inset_0_-8px_15px_#0b1120,inset_0_0_30px_#0b1120] flex">
 						<div className="bg-[#1f2940] w-full flex flex-col gap-3 flex-shrink-0  border border-[#3d5afe] rounded-3xl transition-transform  shadow-lg shadow-[#0b1120]/50 overflow-hidden p-8">
 							<h1 className="sm:text-5xl text-3xl font-bold text-[#33a400]">Info</h1>
-							<div className="grid grid-cols-2">
-								<div className="justify-center flex flex-wrap">
-									<div className="rounded-full border-1 border-[#3d5afe] overflow-hidden h-70 w-70">
+							<div className="flex flex-wrap">
+								<div className="justify-center items-center flex flex-col">
+									<div className="rounded-full border-1 border-[#3d5afe] overflow-hidden aspect-square w-7/10">
 										<img className="object-cover w-full h-full" src={info.pic}/>
 									</div>
-									<div className="w-70">
+									<div className="w-7/10">
 									<h2 className="text-3xl text-[#33a400]">{info.display_name}</h2>
 									<p className="font-thin text-gray-300">{info.real_name}</p>
 									</div>
 								</div>
-								<div className="flex flex-wrap gap-3 mt-4">
+								<div className="flex flex-col flex-wrap gap-3 mt-4">
 									{/* Location */}
 									<div className="flex items-center gap-2">
 										<div className="h-5 w-5">
