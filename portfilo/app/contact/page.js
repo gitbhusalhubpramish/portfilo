1e7a961643d9ffac1c235d196d73cd97f54e677c
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-02-20 20:09:30 +0545

    button

--- portfilo/app/contact/page.js
+++ portfilo/app/contact/page.js
@@ -8,20 +8,24 @@ export default function Contact(){
 	const [loading, setLoading] = useState(false)
 	const handelSubmit = async (e)=>{
 		e.preventDefault()
+		let ok;
+		if (formData.subject===""){
+			ok = confirm("do you want to send message without subject?")
+		}
 		setLoading(true);
-
+		if (ok){
 		try {
 			const form = new FormData();
-        form.append("reply", formData.reply);
-        form.append("message", formData.message);
-        form.append("subject", formData.subject);
+				form.append("reply", formData.reply);
+				form.append("message", formData.message);
+				form.append("subject", formData.subject);
 
-        // Append files
-        files.forEach(file => {
-            form.append("attachment", file);
-        });
-        console.log(form)
-        console.log("stringfy",JSON.stringify(form))
+				// Append files
+				files.forEach(file => {
+						form.append("attachment", file);
+				});
+				console.log(form)
+				console.log("stringfy",JSON.stringify(form))
 			const res = await fetch("/api/contact", {
 				method: "POST",
 				body: form,
@@ -33,14 +37,17 @@ export default function Contact(){
 				setFiles([])
 				setFormData({reply:"", message:"", attachment:files, subject:""});
 				console.log(message, res)
+				alert("Your message was sent successfully.")
 			} else {
 				setMessage("Failed to submit form.");
 				console.log(message, res)
+				alert("something went wrong")
 			}
 		} catch (err) {
 			setMessage("Error: " + err.message);
 			console.log(message)
-		}
+			alert("something went wrong")
+		}}
 
 		console.log(message)
 		setLoading(false);
@@ -120,10 +127,10 @@ export default function Contact(){
 									<div className="px-5 pb-0 w-full">
 									<textarea id ="message" className= "bg-[#0b1120] text-[#e5e7eb]  placeholder-[#94a3b8] border border-[#334155] rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-[#38bdf8] focus:border-[#38bdf8] w-full h-40 justify-start" name="message" placeholder="Your Message" value={formData.message} onChange={(e)=>setFormData({...formData,[e.target.name]:e.target.value})} required/></div>
 									{(files.length>0)&&(
-									<div className="m-5 sm:flex-row flex-wrap flex flex-row">
+									<div className="mx-5 sm:flex-row flex-wrap flex flex-row">
 									{files.map((file,index)=>(
-										<div key={index} className="bg-[#162038] sm:w-1/3 w-full text-[#b0c4de] px-3 py-1 rounded-md border border-[#2e4fff] m-2 text-xl h-10 flex"><p className="flex-1 overflow-hidden">{file.name}</p>
-										<button className="flex justify-center items-center" onClick={()=>setFiles(fls=>fls.filter(item=>item!==file))}>
+										<div key={index} className="bg-[#162038] sm:w-1/3 w-full text-[#b0c4de] px-3 py-1 rounded-md border border-[#2e4fff] m-1 text-xl h-10 flex"><p className="flex-1 overflow-hidden">{file.name}</p>
+										<button type = "button" className="flex justify-center items-center" onClick={()=>setFiles(fls=>fls.filter(item=>item!==file))}>
 											<div className="text-4xl cursor-pointer">
 												&times; 
 											</div>
