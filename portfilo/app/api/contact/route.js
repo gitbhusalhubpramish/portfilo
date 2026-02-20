7eccee08bbb28bc50e8720b4bc670f61cc72fdaa
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-02-20 19:40:17 +0545

    resend but error

--- portfilo/app/api/contact/route.js
+++ portfilo/app/api/contact/route.js
@@ -1,6 +1,19 @@
 import { NextResponse } from "next/server";
+import {Resend} from "resend"
+const resend = new Resend(process.env.RESEND_API_KEY)
+
 export async function POST(req) { 
 	const body = await req.formData(); 
 	console.log(body); 
+	try{
+		await resend.emails.send({
+			from: "Portfolio <onboarding@resend.dev",
+			to: ["bpramish71@gmail.com"],
+			subject: body.get("subject") as string,
+			html: `<p> ${body.get("message")} </p> <p style="margin-top:30px;"> from: ${body.get("reply")}</p>`,
+			body.getAll("attachment") as File[],
+		})
+	}
+	
 	return NextResponse.json({ success: true }); 
 }
