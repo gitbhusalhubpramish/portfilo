cea98963e9ba32165fe499207c0aac5e19654717
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-03-26 15:40:38 +0545

    site map

--- portfilo/next-sitemap.config.js
+++ portfilo/next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://pramishbhusal.com',
+  siteUrl: 'https://pramishbhusal.com.np',
   generateRobotsTxt: true,
   changefreq: 'weekly',
   priority: 0.7,
